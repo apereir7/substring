82418009cc1fb525c6547d30cee0c01c1b67185e
Anthony Pereira <pereiratony@rocektmail.com>
2022-02-10 18:05:31 -0800

    finished

--- substring.rb
+++ substring.rb
@@ -9,12 +9,49 @@ dictionary = ["Below","Down","Go","goIng","yellow", "hello","there", "again", "s
     "mask","freud"
 ]
 
+subset = ["able","a","ask","here"]
 
 
 dictionary.each_with_index do|word, index| 
     "#{index}: #{word.downcase}"
 end
 
-string = "Hello. How are you? 1 2 4 five. Mothaf***a"
 
-p string.downcase.gsub(/[^a-z\s]/i, '').split()
+def separate_str(string)
+    string.downcase.gsub(/[^a-z\s]/i, '').split()
+end
+
+
+
+def low_case(array)
+    array.map do |word|
+        word.downcase
+    end
+end
+
+
+
+def substrings(string,array)
+    new_array = low_case(array)
+    new_string = separate_str(string)
+    hash = Hash.new
+    
+
+    hash_keys = new_string.map do |member|
+        hash[member] = 0
+    end
+
+    new_string.map do |word|
+        hash[word] = new_array.select do |element|
+                        element.include?(word)
+                        end.length() 
+    end
+    return hash
+end
+
+
+
+
+p substrings(" A bunch of random words. Lets see how this goes", dictionary)
+
+
